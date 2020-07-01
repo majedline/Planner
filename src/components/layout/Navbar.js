06f63e811a979e823f89b5ca8e75f5166587668f
Majed A <majed.atwi@gmail.com>
2020-07-01 15:18:23 -0400

    added background image, updated navbar colour, and added the signinlink navlink and signout navlink

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -6,7 +6,7 @@ import SignedOutLinks from './SignedOutLinks';
 
 const Navbar = () => {
     return (
-        <nav className="nav-wrapper grey draken-3">
+        <nav className="nav-wrapper grey darken-1">
             <div className="container">
                 <Link to="/" className="brand-logo">Plan</Link>
                 <SignedInLinks />
