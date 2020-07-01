04e9a68492d73e4fdcfd84b79bf761b7d99845e3
Majed A <majed.atwi@gmail.com>
2020-07-01 14:10:58 -0400

    Added the template for the dashboard, the notification section, and projectList

---
+++ src/components/project/ProjectList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const ProjectList = () => {
+
+    return(
+      <div className="project-lis section">
+          <div className=" card z-depth-0 project-summary">
+              <div className="card-content grey-text text-darken-3">
+                  <span className="card-title">Project Title</span>
+                  <p>posted by xyz</p>
+                  <p className="grey-text">3rd of sept 2 am</p>
+              </div>
+          </div>
+
+
+          <div className=" card z-depth-0 project-summary">
+              <div className="card-content grey-text text-darken-3">
+                  <span className="card-title">Project Title</span>
+                  <p>posted by xyz</p>
+                  <p className="grey-text">3rd of sept 2 am</p>
+              </div>
+          </div>
+
+          <div className=" card z-depth-0 project-summary">
+              <div className="card-content grey-text text-darken-3">
+                  <span className="card-title">Project Title</span>
+                  <p>posted by xyz</p>
+                  <p className="grey-text">3rd of sept 2 am</p>
+              </div>
+          </div>
+
+      </div>  
+    );
+
+}
+
+export default ProjectList;
