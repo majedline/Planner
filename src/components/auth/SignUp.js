cb18dcc5482fa95ce2de5d6738b53c15bdadec28
Majed A <majed.atwi@gmail.com>
2020-07-04 16:47:10 -0400

    Added the signup code in the signup js and respective actions to firestore and reducers

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { signUp } from '../../store/actions/authActions';
 
 class SignUp extends Component {
     state = {
@@ -14,12 +15,12 @@ class SignUp extends Component {
         this.setState({
             [e.target.id]: e.target.value
         });
-        console.log(e);
     }
 
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+        this.props.signUp(this.state);
+        
     }
 
     render() {
@@ -65,4 +66,10 @@ const mapStateToProps = (state) => {
     };
 }
 
-export default connect(mapStateToProps)(SignUp);
+const mapDispatchToProps = (dispatch) =>{
+    return{
+        signUp: (newUser) => dispatch(signUp(newUser))
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp);
--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -21,7 +21,34 @@ export const signOut = () => {
         firebase.auth().signOut().then(() => {
             dispatch({ type: "SIGNOUT_SUCCESS" })
         });
-
     }
+}
 
+export const signUp = (newUser) => {
+    return (dispatch, getState, { getFirebase, getFirestore }) => {
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+
+        // creating a new user
+        firebase.auth().createUserWithEmailAndPassword(
+            newUser.email,
+            newUser.password
+
+        ).then((creatingNewUserServerResponse) => {
+
+            const usersCollection = firestore.collection('user')
+            // create a new doc with that id
+            const document = usersCollection.doc(creatingNewUserServerResponse.user.uid);
+            // store the data insite the document
+            document.set({
+                firstName: newUser.firstName,
+                lastName: newUser.lastName,
+                initials: newUser.firstName[0] + newUser.lastName[0]
+            }).then(() => {
+                dispatch({ type: 'SIGNUP_SUCCESS' });
+            }).catch((error) => {
+                dispatch({ type: 'SIGNUP_ERROR', err: error});
+            })
+        })
+    };
 }
