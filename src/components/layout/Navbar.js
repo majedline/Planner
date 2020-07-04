6abaaecc31c1e1eee3032229a073c010561e859c
Majed A <majed.atwi@gmail.com>
2020-07-04 01:49:18 -0400

    changed the Navbar to be based on if logged in or not based on auth.id

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -2,21 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import SignedInLinks from './SignedInLink';
 import SignedOutLinks from './SignedOutLinks';
+import { connect } from 'react-redux';
 
 
 
-const Navbar = () => {
+const Navbar = (props) => {
+
+    const { auth } = props;
+    const links = auth.uid ? <SignedInLinks /> : <SignedOutLinks />
+
     return (
         <nav className="nav-wrapper grey darken-1">
             <div className="container">
                 <Link to="/" className="brand-logo">Plan</Link>
-                <SignedInLinks />
-                <SignedOutLinks />
+                {links}
             </div>
         </nav>
 
     );
 }
 
+const mapStateToProps = (state) => {
+    return {
+        auth: state.firebase.auth
+    };
 
-export default Navbar;
+}
+
+export default connect(mapStateToProps)(Navbar);
