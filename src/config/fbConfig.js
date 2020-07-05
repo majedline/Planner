2d009419ceceff3f61ce1eea2a8a817629875781
Majed A <majed.atwi@gmail.com>
2020-07-05 14:21:32 -0400

    updated config

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -6,14 +6,14 @@ import 'firebase/auth';
 
 // Your web app's Firebase configuration
 var config = {
-    apiKey: "AIzaSyBz2uQcFqTAfUoCFNdjg47kYQfnvc_pur4",
+    apiKey: "xxxxxxxxxxxxxxxxxxxxx",
     authDomain: "net-planner-7be21.firebaseapp.com",
-    databaseURL: "https://net-planner-7be21.firebaseio.com",
+    databaseURL: "xxxxxxxxxxxxx",
     projectId: "net-planner-7be21",
     storageBucket: "net-planner-7be21.appspot.com",
-    messagingSenderId: "8503679274",
-    appId: "1:8503679274:web:7cbd10a6456e1fbe15a653",
-    measurementId: "G-FF4ZFQ8WMY"
+    messagingSenderId: "xxxxxxxxxxxxxx",
+    appId: "xxxxxxxxxxxxxxxxx",
+    measurementId: "xxxxxxxxxxxx"
 };
 // Initialize Firebase
 firebase.initializeApp(config);
